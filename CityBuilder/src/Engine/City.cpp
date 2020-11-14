dfbfe39217636a7a2fad37c730820be04b0f8cac
msearles25 <skaman4459@gmail.com>
2020-11-14 13:36:17 -0500

    City update method!

--- CityBuilder/src/Engine/City.cpp
+++ CityBuilder/src/Engine/City.cpp
@@ -16,7 +16,7 @@ double City::distributePool(double& pool, Tile& tile, double rate)
 
 	if (pool > 0)
 	{
-		int moving{ maxPop - tile.m_population };
+		int moving{ int(maxPop - tile.m_population) };
 		if (moving > moveRate) moving = moveRate;
 		if (pool - moving < 0) moving = pool;
 		pool -= moving;
@@ -125,6 +125,149 @@ void City::save(std::string cityName)
 
 void City::update(float dt)
 {
+	double popTotal{ 0 };
+	double commercialRevenue{ 0 };
+	double industrialRevenue{ 0 };
+
+	// Update the game time
+	currentTime += dt;
+
+	if (currentTime < timePerDay) return;
+	++day;
+
+	currentTime = 0.0;
+	if (day % 30 == 0)
+	{
+		funds += earnings;
+		earnings = 0;
+	}
+
+	// Run first pass of tile updates. Handles pool distribution
+	for (int i{ 0 }; i < map.m_tiles.size(); ++i)
+	{
+		Tile& tile{ map.m_tiles[shuffledTiles[i]] };
+
+		if (tile.m_tileType == TileType::RESIDENTIAL)
+		{
+			// Redistribute pool and increase pop total by the tile's pop
+			distributePool(populationPool, tile, birthRate - deathRate);
+
+			popTotal += tile.m_population;
+		}
+		else if (tile.m_tileType == TileType::COMMERCIAL)
+		{
+			// Hire
+			if (rand() % 100 < 15 * (1.0 - commercialTax))
+			{
+				distributePool(employmentPool, tile, 0.00);
+			}
+		}
+		else if (tile.m_tileType == TileType::INDUSTRIAL)
+		{
+			// Extract resources from ground
+			if (map.m_resources[i] > 0 && rand() % 100 < population)
+			{
+				++tile.m_production;
+				--map.m_resources[i];
+			}
+
+			// Hire
+			if (rand() % 100 < 15 * (1.0 - industrialTax))
+			{
+				distributePool(employmentPool, tile, 0.00);
+			}
+		}
+
+		tile.update();
+	}
+
+	// Goods manufacturing
+	for (int i{ 0 }; i < map.m_tiles.size(); ++i)
+	{
+		Tile& tile{ map.m_tiles[shuffledTiles[i]] };
+
+		if (tile.m_tileType == TileType::INDUSTRIAL)
+		{
+			int recievedResources{ 0 };
+
+			for (auto& tile2 : map.m_tiles)
+			{
+				if (tile2.m_regions[0] == tile.m_regions[0] && tile2.m_tileType == TileType::INDUSTRIAL)
+				{
+					if (tile2.m_production > 0)
+					{
+						++recievedResources;
+						--tile2.m_production;
+					}
+					if (recievedResources >= tile.m_tileVariant + 1) break;
+				}
+			}
+
+			// turn resources into goods
+			tile.m_storedGoods += (recievedResources + tile.m_production) * (tile.m_tileVariant + 1);
+		}
+	}
+
+	// Goods distribution
+	for (int i{ 0 }; i < map.m_tiles.size(); ++i)
+	{
+		Tile& tile{ map.m_tiles[shuffledTiles[i]] };
+
+		if (tile.m_tileType == TileType::COMMERCIAL)
+		{
+			int recievedGoods{ 0 };
+			double maxCustomers{ 0.0 };
+
+			for (auto& tile2 : map.m_tiles)
+			{
+				if (tile2.m_regions[0] == tile.m_regions[0] &&
+					tile2.m_tileType == TileType::INDUSTRIAL &&
+					tile2.m_storedGoods > 0)
+				{
+					while (tile2.m_storedGoods > 0 && recievedGoods != tile.m_tileVariant + 1)
+					{
+						--tile2.m_storedGoods;
+						++recievedGoods;
+						industrialRevenue += 100 * (1.0 - industrialTax);
+					}
+				}
+				else if (tile2.m_regions[0] == tile.m_regions[0] &&
+					tile2.m_tileType == TileType::RESIDENTIAL)
+				{
+					maxCustomers += tile2.m_population;
+				}
+				if (recievedGoods == tile.m_tileVariant + 1) break;
+			}
+
+			tile.m_production = (recievedGoods * 100.0 + rand() % 20) * (1.0 - commercialTax);
+
+			double revenue{ tile.m_production * maxCustomers * tile.m_population / 100.0 };
+
+			commercialRevenue += revenue;
+		}
+	}
+
+	// Adjust population pool for deaths/births
+	populationPool += populationPool * (birthRate - deathRate);
+	popTotal += populationPool;
+
+	// Adjust employment pool
+	float newWorkers{ (popTotal - population) * propCanWork };	
+	newWorkers *= newWorkers < 0 ? -1 : 1;
+
+	employmentPool += newWorkers;
+	employable += newWorkers;
+
+	if (employmentPool < 0)employmentPool = 0;
+	if (employable < 0) employable = 0;
+	
+	// update city pop
+	population = popTotal;
+
+	// calc city income from the tax
+	earnings = (population - populationPool) * 15 * residentialTax;
+	earnings += commercialRevenue * commercialTax;
+	earnings += industrialRevenue * industrialTax;
 }
 
 void City::bulldoze(const Tile& tile)
