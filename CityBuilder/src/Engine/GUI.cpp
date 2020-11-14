16cf003be5aee21ea5c4bae8a39f5c6aa364a229
msearles25 <skaman4459@gmail.com>
2020-11-13 22:39:13 -0500

    Gui class

--- CityBuilder/src/Engine/GUI.cpp
+++ CityBuilder/src/Engine/GUI.cpp
@@ -1,1 +1,29 @@
 #include "GUI.h"
+
+GUI::GUI(sf::Vector2f dimensions, int padding, bool horizontal, GuiStyle& style, std::vector<std::pair<std::string, std::string>> entries)
+{
+	m_visible = false;
+	m_horizontal = horizontal;
+	m_style = style;
+	m_dimensions = dimensions;
+	m_padding = padding;
+
+	// Construct the background shape
+	sf::RectangleShape shape;
+	shape.setSize(m_dimensions);
+	shape.setFillColor(style.m_bodyCol);
+	shape.setOutlineThickness(-style.m_borderSize);
+	shape.setOutlineColor(style.m_borderCol);
+
+	// Construct each entry
+	for (auto entry : entries)
+	{
+		sf::Text text;
+		text.setString(entry.first);
+		text.setFont(*style.m_font);
+		text.setColor(style.m_textCol);
+		text.setCharacterSize(dimensions.y - style.m_borderSize - m_padding);
+
+		m_entries.push_back(GuiEntry(entry.second, shape, text));
+	}
+}
--- CityBuilder/src/Engine/GUI.h
+++ CityBuilder/src/Engine/GUI.h
@@ -35,7 +35,63 @@ public:
 	GuiStyle() { }
 };
 
-class GUI
+class GuiEntry
 {
+public:
+	sf::RectangleShape m_shape;
+	std::string m_message;
+	sf::Text m_text;
 
+	GuiEntry(const std::string& message, sf::RectangleShape shape, sf::Text text)
+	{
+		m_message = message;
+		m_shape = shape;
+		m_text = text;
+	}
+
+	GuiEntry() { }
+};
+
+class GUI : public sf::Transformable, public sf::Drawable
+{
+private:
+	// if true, the entries will be horizontally adjacent
+	bool m_horizontal;
+
+	GuiStyle m_style;
+	
+	sf::Vector2f m_dimensions;
+	int m_padding;
+public:
+	std::vector<GuiEntry> m_entries;
+	bool m_visible;
+
+	GUI(sf::Vector2f dimensions, int padding, bool horizontal,
+		GuiStyle& style, std::vector<std::pair<std::string, std::string>> entries);
+
+	sf::Vector2f getSize();
+
+	/*
+	Return the entry that the mouse is hovering over. 
+	-1 is returned if the mouse is outside of the gui
+	*/
+	int getEntry(const sf::Vector2f mousePos);
+
+	// Change the text of an entry
+	void setEntryText(int entry, std::string text);
+
+	void setDimensions(sf::Vector2f dimensions);
+
+	// Draw the menu
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+	void show();
+	void hide();
+
+	// Highlights an entry of the menu
+	void highlight(const int entry);
+
+	// Return the message that's bound to the entry
+	std::string activate(const int entry);
+	std::string activate(const sf::Vector2f mousePos);
 };
