7ca6d69c21801acf306a23e4f6b95087125bd9b2
msearles25 <skaman4459@gmail.com>
2020-11-14 10:26:38 -0500

    City distributePool method

--- CityBuilder/src/Engine/City.cpp
+++ CityBuilder/src/Engine/City.cpp
@@ -1,4 +1,37 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
 #include "City.h"
+#include "Tile.h"
+
+double City::distributePool(double& pool, Tile& tile, double rate)
+{
+	const static int moveRate{ 4 };
+	unsigned int maxPop{ tile.m_maxPopPerLevel * (tile.m_tileVariant + 1) };
+
+	if (pool > 0)
+	{
+		int moving{ maxPop - tile.m_population };
+		if (moving > moveRate) moving = moveRate;
+		if (pool - moving < 0) moving = pool;
+		pool -= moving;
+		tile.m_population += moving;
+	}
+
+	tile.m_population += tile.m_population * rate;
+
+	if (tile.m_population > maxPop)
+	{
+		pool += tile.m_population - maxPop;
+		tile.m_population = maxPop;
+	}
+
+	return tile.m_population;
+}
 
 City::City()
 {
@@ -17,6 +50,30 @@ City::City()
 	currentTime = 0.0f;
 	timePerDay = 1.0f;
 	day = 0;
+}
+
+void City::load(std::string cityName, std::map<std::string, Tile>& tileAtlas)
+{
+}
+
+void City::save(std::string cityName)
+{
+}
+
+void City::update(float dt)
+{
+}
+
+void City::bulldoze(const Tile& tile)
+{
+}
+
+void City::shuffleTiles()
+{
+}
+
+void City::tileChanged()
+{
 }
 
 double City::getHomeless()
--- CityBuilder/src/Engine/City.h
+++ CityBuilder/src/Engine/City.h
@@ -51,4 +51,6 @@ public:
 	void shuffleTiles();
 	void tileChanged();
 
+	double getHomeless();
+	double getUnemployed();
 };
