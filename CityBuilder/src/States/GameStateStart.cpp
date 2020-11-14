a113ef0199df7bc5b018c88b3c11e0f57cd05238
msearles25 <skaman4459@gmail.com>
2020-11-14 00:09:10 -0500

    added functionality to load game button

--- CityBuilder/src/States/GameStateStart.cpp
+++ CityBuilder/src/States/GameStateStart.cpp
@@ -25,29 +25,63 @@ void GameStateStart::update(const float dt)
 void GameStateStart::handleInput()
 {
 	sf::Event event;
+
+	sf::Vector2f mousePos{ m_game->window.mapPixelToCoords(
+		sf::Mouse::getPosition(m_game->window), m_view) };
+
 	while (m_game->window.pollEvent(event))
 	{
 		switch (event.type)
 		{
 			// close the window
 		case sf::Event::Closed:
+		{
 			m_game->window.close();
 			break;
+		}
 			// Resize the window
 		case sf::Event::Resized:
+		{
 			m_view.setSize(event.size.width, event.size.height);
 			m_game->background.setPosition(m_game->window.mapPixelToCoords(sf::Vector2i(0, 0)));
+
+			sf::Vector2f pos{ sf::Vector2f(event.size.width, event.size.height) };
+			pos *= 0.5f;
+			pos = m_game->window.mapPixelToCoords(sf::Vector2i(pos), m_view);
+
+			guiSystem.at("menu").setPosition(pos);
+	
 			m_game->background.setScale(
 				float(event.size.width) / float(m_game->background.getTexture()->getSize().x),
 				float(event.size.height) / float(m_game->background.getTexture()->getSize().y));
+
 			break;
+		}
 			// Check if key is pressed
+		case sf::Event::MouseMoved:
+		{
+			guiSystem.at("menu").highlight(guiSystem.at("menu").getEntry(mousePos));
+			break;
+		}
+		case sf::Event::MouseButtonPressed:
+		{
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+				std::string msg{ guiSystem.at("menu").activate(mousePos) };
+
+				if (msg == "load_game")
+				{
+					loadGame();
+				}
+			}
+			break;
+		}
 		case sf::Event::KeyPressed:
+		{
 			if (event.key.code == sf::Keyboard::Escape)
 				m_game->window.close();
-			else if (event.key.code == sf::Keyboard::Space)
-				loadGame();
 			break;
+		}
 		default:
 			break;
 		}
